7df947d742992259448f9a90daf80a2d83101a6f
hrimfaxi <outmatch@gmail.com>
2016-05-24 23:07:52 +0800

    autoproxy-ng: 0.5.2
    I messed up version number, sorry!
    
    Signed-off-by: hrimfaxi <outmatch@gmail.com>

--- gen_rev.sh
+++ gen_rev.sh
@@ -2,6 +2,7 @@
 
 GITTAG=$(git describe --tag)
 
+sed -i "s/{{VERSION}}/${GITTAG}/g" install.rdf
 sed -i "s/{{VERSION}}/${GITTAG}/g" package.json
 sed -i "s/{{VERSION}}/${GITTAG}/g" components/AutoProxy.js
 sed -i "s/{{BUILD}}/${GITTAG}/g" components/AutoProxy.js
